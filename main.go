c119dafbbe7328304c77fb59f9af85fa1b3738c1
Yb2411 <132000325+Yb2411@users.noreply.github.com>
2023-06-29 23:40:01 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,14 +1,35 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"net/http"
+	"os"
+
+	_ "github.com/lib/pq" // Here is your postgres driver
+)
+
+const (
+	host     = os.Getenv("DB_HOST")
+	port     = os.Getenv("DB_PORT")
+	user     = os.Getenv("DB_USER")
+	password = os.Getenv("DB_PASS")
+	dbname   = os.Getenv("DB_NAME")
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, world!")
-	})
+	fmt.Println("Trying to connect..")
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open(user, psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 
-	http.ListenAndServe(":8080", nil)
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
 }
